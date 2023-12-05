8a3ca3b0b606269aeb1b145161380bb557a461ee
Nelson Luis Moreira da Costa <nelson.costa@universidadeeuropeia.pt>
2023-12-05 14:17:52 +0000

    Added Ingame Gamemode Skeleton To Showcase How To handshake and allow players or place them in the right location

---
+++ FPS_Matchmade/Source/FPS_Matchmade/IngameDeathmatchGamemode.cpp
@@ -0,0 +1,36 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "IngameDeathmatchGamemode.h"
+
+void AIngameDeathmatchGamemode::BeginPlay()
+{
+	Super::BeginPlay();
+}
+
+void AIngameDeathmatchGamemode::Tick(float DeltaSeconds)
+{
+	Super::Tick(DeltaSeconds);
+}
+
+void AIngameDeathmatchGamemode::EndPlay(const EEndPlayReason::Type EndPlayReason)
+{
+	Super::EndPlay(EndPlayReason);
+}
+
+void AIngameDeathmatchGamemode::PreLogin(const FString& Options, const FString& Address,
+	const FUniqueNetIdRepl& UniqueId, FString& ErrorMessage)
+{
+	Super::PreLogin(Options, Address, UniqueId, ErrorMessage);
+}
+
+APlayerController* AIngameDeathmatchGamemode::Login(UPlayer* NewPlayer, ENetRole InRemoteRole, const FString& Portal,
+	const FString& Options, const FUniqueNetIdRepl& UniqueId, FString& ErrorMessage)
+{
+	return Super::Login(NewPlayer, InRemoteRole, Portal, Options, UniqueId, ErrorMessage);
+}
+
+void AIngameDeathmatchGamemode::PostLogin(APlayerController* NewPlayer)
+{
+	Super::PostLogin(NewPlayer);
+}
---
+++ FPS_Matchmade/Source/FPS_Matchmade/IngameDeathmatchGamemode.h
@@ -0,0 +1,86 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameMode.h"
+#include "IngameDeathmatchGamemode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class FPS_MATCHMADE_API AIngameDeathmatchGamemode final : public AGameMode
+{
+	GENERATED_BODY()
+
+	virtual void BeginPlay() override;
+	virtual void Tick(float DeltaSeconds) override;
+	virtual void EndPlay(const EEndPlayReason::Type EndPlayReason) override;
+
+	// Login Functions
+	// NOTE: We use these to login and initialize player options
+
+		/**
+	 * Accept or reject a player attempting to join the server.  Fails login if you set the ErrorMessage to a non-empty string.
+	 * PreLogin is called before Login.  Significant game time may pass before Login is called
+	 *
+	 * @param	Options					The URL options (e.g. name/spectator) the player has passed
+	 * @param	Address					The network address of the player
+	 * @param	UniqueId				The unique id the player has passed to the server
+	 * @param	ErrorMessage			When set to a non-empty value, the player will be rejected using the error message set
+	 */
+	virtual void PreLogin(const FString& Options, const FString& Address, const FUniqueNetIdRepl& UniqueId, FString& ErrorMessage);
+
+	/**
+	 * Called to login new players by creating a player controller, overridable by the game
+	 *
+	 * Sets up basic properties of the player (name, unique id, registers with backend, etc) and should not be used to do
+	 * more complicated game logic.  The player controller is not fully initialized within this function as far as networking is concerned.
+	 * Save "game logic" for PostLogin which is called shortly afterward.
+	 *
+	 * @param NewPlayer pointer to the UPlayer object that represents this player (either local or remote)
+	 * @param RemoteRole the remote role this controller has
+	 * @param Portal desired portal location specified by the client
+	 * @param Options game options passed in by the client at login
+	 * @param UniqueId platform specific unique identifier for the logging in player
+	 * @param ErrorMessage [out] error message, if any, why this login will be failing
+	 *
+	 * If login is successful, returns a new PlayerController to associate with this player. Login fails if ErrorMessage string is set.
+	 *
+	 * @return a new player controller for the logged in player, NULL if login failed for any reason
+	 */
+	virtual APlayerController* Login(UPlayer* NewPlayer, ENetRole InRemoteRole, const FString& Portal, const FString& Options, const FUniqueNetIdRepl& UniqueId, FString& ErrorMessage);
+
+	/** Called after a successful login.  This is the first place it is safe to call replicated functions on the PlayerController. */
+	virtual void PostLogin(APlayerController* NewPlayer);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+};
